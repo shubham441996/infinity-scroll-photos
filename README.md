8658790ffc3ba1b55be4bf2319cb52791ca55ed6
Shubham Paliwal <shubham.paliwal@getfareye.com>
2020-08-15 23:22:38 +0530

    feat:

---
+++ README.md
@@ -0,0 +1,29 @@
+# Random Quote Generator
+
+## Tech Stack Used
+
+1. HTML
+2. Css
+3. Javascript
+
+## Free Unsplash Api Used
+
+[Unsplash Api](https://api.unsplash.com/photos/random)
+
+## Special Thanks
+
+[Andrei Neagoie](https://twitter.com/andreineagoie?lang=en)
+
+New Things Learn
+
+- How to make images responsive
+
+`.responsive-img {
+  height: auto;
+  border-radius: 3px;
+  object-fit: cover;
+  width: 100%;
+  height: 250px;
+}`
+
+- How to lazy load images
--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("Hello World");
+let isReady = false;
+let imageLoadCount = 0;
+let dataCount = 0;
+let count = 10;
+let loader = document.getElementById("loader");
+
+async function fetchPhotos() {
+  try {
+    loader.hidden = false;
+    let clientId = "z5OgDAa1rOjH46T5ybkduWdkjE9S2KpEwcjMoC0hsp8";
+    let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${clientId}&count=${count}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    dataCount = data.length;
+    renderImage(data);
+    loader.hidden = true;
+  } catch (ex) {}
+}
+
+function renderImage(data = []) {
+  imageLoadCount = 0;
+  let ele = document.getElementById("image-container");
+  data.forEach(({ urls: { regular }, alt_description }) => {
+    let divEle = document.createElement("div");
+    divEle.className = "image";
+    let image = document.createElement("img");
+    image.className = "responsive-img";
+    image.src = regular;
+    image.alt = alt_description;
+    image.addEventListener("load", imageLoad);
+    let figcaption = document.createElement("figcaption");
+    figcaption.textContent = alt_description;
+    divEle.appendChild(image);
+    divEle.appendChild(figcaption);
+    ele.appendChild(divEle);
+  });
+}
+function imageLoad() {
+  imageLoadCount++;
+  if (dataCount === imageLoadCount) {
+    isReady = true;
+  }
+}
+window.onscroll = function (el) {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+    isReady
+  ) {
+    isReady = false;
+    //console.log(window.innerHeight, window.scrollY, document.body.offsetHeight);
+
+    fetchPhotos();
+  }
+};
+fetchPhotos();
